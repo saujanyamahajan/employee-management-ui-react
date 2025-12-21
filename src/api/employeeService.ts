7ca8d3357c9a1250203208d41bdf6c1040e48e4b
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-21 20:48:15 +0530

    Ft: delete employee by id

--- src/api/employeeService.ts
+++ src/api/employeeService.ts
@@ -9,7 +9,10 @@ const apiInstance = axios.create({
 
 const ApiEndpoints = {
   GET_EMPLOYEES: "/employee",
-  GET_EMPLOYEE_BY_ID: "/employee"
+  GET_EMPLOYEE_BY_ID: "/employee",
+  DELETE_EMPLOYEE_BY_ID:"/employee",
+  INSERT_EMPLOYEE:"/employee",
+  UPDATE_EMPLOYEE_BY_ID:"/employee"
 } as const;
 
 export const getEmployees = async (): Promise<Employee[]> => {
@@ -19,4 +22,7 @@ export const getEmployees = async (): Promise<Employee[]> => {
 export const getEmployeeById = async (id: number): Promise<Employee> => {
   const response = await apiInstance.get<Employee>(`${ApiEndpoints.GET_EMPLOYEE_BY_ID}/${id}`);
   return response.data;
-}
+};
+export const deleteEmployeeById = async (id: number): Promise<void> => {
+  await apiInstance.delete(`${ApiEndpoints.DELETE_EMPLOYEE_BY_ID}/${id}`);
+};
