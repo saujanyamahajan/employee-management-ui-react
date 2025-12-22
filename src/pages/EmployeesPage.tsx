42d6df46c24494d97c52afe9136995154fca4cab
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-22 23:17:07 +0530

    Ft: added add employee btn

--- src/pages/EmployeesPage.tsx
+++ src/pages/EmployeesPage.tsx
@@ -3,9 +3,28 @@ import "./EmployeePage.css";
 
 const EmployeesPage = () => {
   return (
-      <div className="employee-page">
-        <h5>Employees</h5>
-      <EmployeeList />
+     <div className="employees-page">
+      {/* Header */}
+      <div className="header">
+                <div className="header-actions">
+        <div>
+          <h1>Employees</h1>
+          {/* <p>{filteredEmployees.length} total employees</p> */}
+        </div>
+        <button className="add-btn">+ Add Employee</button>
+        </div>
+
+         {/* Search Box */}
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search employees by name, email, department, or role..."
+          // value={searchTerm}
+          // onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+                  <EmployeeList />
+      </div>
     </div>
   );
 };
