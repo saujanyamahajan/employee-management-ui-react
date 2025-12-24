74fcd404e3cffe35f20f66fc873d1cb15c86e18c
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-24 10:30:00 +0530

    fix: omit employee id from insert employee api

--- src/api/employeeService.ts
+++ src/api/employeeService.ts
@@ -19,6 +19,7 @@ export const getEmployees = async (): Promise<Employee[]> => {
   const response = await apiInstance.get<Employee[]>(ApiEndpoints.GET_EMPLOYEES);
   return response.data;
 };
+// export const getEmployeeById = async (id: number) => {}
 export const getEmployeeById = async (id: number): Promise<Employee> => {
   const response = await apiInstance.get<Employee>(`${ApiEndpoints.GET_EMPLOYEE_BY_ID}/${id}`);
   return response.data;
@@ -26,10 +27,15 @@ export const getEmployeeById = async (id: number): Promise<Employee> => {
 export const deleteEmployeeById = async (id: number): Promise<void> => {
   await apiInstance.delete(`${ApiEndpoints.DELETE_EMPLOYEE_BY_ID}/${id}`);
 };
-export const insertEmployee = async(employee: CreateEmployee) : Promise<Employee[]>=>{
-    const response = await apiInstance.post<Employee[]>(ApiEndpoints.INSERT_EMPLOYEE,employee);
+export const insertEmployee = async (
+  employee: CreateEmployee
+): Promise<Employee> => {
+  const response = await apiInstance.post<Employee>(
+    ApiEndpoints.INSERT_EMPLOYEE,
+    employee
+  );
   return response.data;
-}
+};
 export const updateEmployee= async(id: number,employee: Employee):Promise<Employee>=>{
     const response = await apiInstance.put<Employee>(`${ApiEndpoints.UPDATE_EMPLOYEE_BY_ID}/${id}`,employee);
   return response.data;
