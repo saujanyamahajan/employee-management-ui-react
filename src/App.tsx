17a4ee107692d3f18accb9b376a8d60cb3e8dbbc
saujanyamahajan <saujanya.9919@gmail.com>
2026-01-01 18:10:17 +0530

    chore: add ppl icon in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 // import { Routes, Route, Navigate } from "react-router-dom";
 import EmployeesPage from "./pages/EmployeesPage";
 import "./App.css";
+import peopleIcon from "../src/assets/people.svg";
 
 function App() {
   return (
     <>
-      <div className="navBar">
-        <h2>Employee Management Portal</h2>
-        <h4 className="subHeading">Manage your team members</h4>
+      <div className="navBar flex gap-3">
+        <img src={peopleIcon} className="h-10 w-10 mt-2" />
+        <div>
+          <h2>Employee Management Portal</h2>
+          <h4 className="subHeading">Manage your team members</h4>
+        </div>
       </div>
       <EmployeesPage />
     </>
